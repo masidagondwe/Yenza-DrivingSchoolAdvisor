4260e613ee2ca7b0897eb9d5e7c69e754bfacd5a
masidagondwe <masidahgondwe@gmail.com>
2020-05-04 21:29:23 +0200

    Home - main feed update

---
+++ app/src/main/java/com/temwatech/yenza/util/FirebaseUtil.java
@@ -0,0 +1,114 @@
+package com.temwatech.yenza.util;
+
+import android.app.ListActivity;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.temwatech.yenza.model.DrivingSchool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+
+public class FirebaseUtil {
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseStorage mStorage;
+    public static StorageReference mStorageRef;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+    public static ArrayList<DrivingSchool> mSchools;
+    private static final int RC_SIGN_IN = 123;
+  //  private static ListActivity caller;
+
+    private FirebaseUtil(){};
+    public static boolean isSchoolOwner;
+
+
+    public static void openFbReference(String ref, final ListActivity callerActivity) {
+        if (firebaseUtil == null) {
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
+           // caller = callerActivity;
+
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.signIn();
+                    }
+                    else {
+                        String userId = firebaseAuth.getUid();
+                        checkAdmin(userId);
+                    }
+                    Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
+                }
+            };
+            connectStorage();
+
+        }
+
+        mSchools = new ArrayList<DrivingSchool>();
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    private static void signIn() {
+
+    }
+
+    private static void checkAdmin(String uid) {
+        FirebaseUtil.isSchoolOwner = false;
+        DatabaseReference ref = mFirebaseDatabase.getReference().child("administrators")
+                .child(uid);
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                FirebaseUtil.isSchoolOwner = true;
+               // caller.showMenu();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
+    }
+
+    public static void attachListener() {
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+    public static void detachListener() {
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
+    }
+    public static void connectStorage() {
+        mStorage = FirebaseStorage.getInstance();
+        mStorageRef = mStorage.getReference().child("deals_pictures");
+    }
+}
