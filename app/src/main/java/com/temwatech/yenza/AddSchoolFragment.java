87394e59df535cdaa1732b9fc7c4ef207821a5ca
masidagondwe <masidahgondwe@gmail.com>
2020-06-09 22:28:03 +0200

    Home - main feed update - clean up

--- app/src/main/java/com/temwatech/yenza/AddSchoolFragment.java
+++ app/src/main/java/com/temwatech/yenza/AddSchoolFragment.java
@@ -19,13 +19,9 @@ import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddSchoolFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddSchoolFragment extends Fragment {
 
+public class AddSchoolFragment extends Fragment {
+    private static final String TAG = "AddSchoolFragment";
 
     //
     private static final int SELECT_PICTURES = 10 ;
@@ -38,43 +34,12 @@ public class AddSchoolFragment extends Fragment {
     int up = 0;
     int k =0;
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddSchoolFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddSchoolFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddSchoolFragment newInstance(String param1, String param2) {
-        AddSchoolFragment fragment = new AddSchoolFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
